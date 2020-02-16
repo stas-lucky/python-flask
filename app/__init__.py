22bea317c6042e6d635932259425bcee4e019e1c
Stanislav R <per_mail@ua.fm>
2020-02-16 19:35:23 +0100

    more 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -11,4 +15,37 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 
-from app import routes, models
+from app import routes, models, errors
+
+
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="studio-test@simplifai.ai",
+            toaddrs=app.config["ADMINS"], subject="Microblog Failure",
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    # File logger
+    if not app.debug:
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+        file_handler = RotatingFileHandler("logs/logs=.log", maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("Microblog startup")
--- app/models.py
+++ app/models.py
@@ -10,6 +10,12 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+followers = db.Table("followers",
+                     db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+                     db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
+                     )
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -19,6 +25,12 @@ class User(UserMixin, db.Model):
 
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    followed = db.relationship("User", secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref("followers", lazy="dynamic"), lazy="dynamic"
+                               )
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -43,4 +55,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return "Post: {}".format(self.body)
+
+
 
