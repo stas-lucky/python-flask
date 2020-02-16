6fa8cc808857e814bcd830dd357f4cd0bb9a3470
Stanislav R <per_mail@ua.fm>
2020-02-16 09:09:20 +0100

    more 4

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 @login.user_loader
@@ -16,6 +17,9 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
 
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -24,6 +28,11 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return "User {}".format(self.username)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
+            digest, size)
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, send_from_directory, url_for, request
 from flask_login import current_user, login_user, login_required, logout_user
 from app import app, db
 
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from werkzeug.urls import url_parse
 
@@ -16,7 +18,6 @@ def send_styles(path):
 @app.route('/index')
 @login_required
 def index():
-    user = {"username": "Stanislav"}
     posts = [
         {
             'author': {'username': 'John'},
@@ -69,3 +70,39 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "Test post #1"},
+        {"author": user, "body": "Test post #2"}
+    ]
+
+    return render_template("user.html", title="Profile", user=user, posts=posts)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
